8375d6e2c9da6a882562f50e097ad9f827396cb7
peterlamphere <35402159+peterlamphere@users.noreply.github.com>
2018-01-13 11:21:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prototype-Command-Based-Robot
---
+++ src/org/usfirst/frc/team6873/robot/subsystems/DriveSystem.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team6873.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;  
+	
+
+/**
+ *
+ */
+public class DriveSystem extends Subsystem {
+	CANTalon  frontLeftMotor = new CANTalon (2),  rearLeftMotor = new CANTalon(1), 
+			  frontRightMotor = new CANTalon(3),  rearRightMotor = new CANTalon(4); 
+
+new RobotDrive( frontLeftMotor,  rearLeftMotor,  frontRightMotor,  rearRightMotor );
+
+    // Put methods forthod controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+}
+
